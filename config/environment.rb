a4d1b88243f2903c9ffa50b2dc432df1be18eb85
Sean Seefried <sean.seefried@kinesis.org>
2009-11-20 15:52:00 +1100

    Basic set up

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "roo", :version => '>=1.9.1'
+  config.gem 'roo', :version => '>=1.9.1'
+  config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
